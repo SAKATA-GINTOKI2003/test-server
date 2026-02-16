5611d6756b6b3c100348023aa53fcae728224bdb
SAKATA-GINTOKI2003 <147246417+SAKATA-GINTOKI2003@users.noreply.github.com>
2026-02-16 17:08:43 +0530

    update env

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT;
+console.log(process.env.port);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.get("/test", (req, res) => {
+  res.send("<h1>Hello World Running Test Server</h1>");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
